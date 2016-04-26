9d2de2eb681b8e46b1c48ae1c8d8b1f84c90eb05
David Yeung <quxbaz@gmail.com>
2016-04-26 13:29:16 -0400

    Removed nodejs parts

--- uuid.js
+++ uuid.js
@@ -50,27 +50,10 @@
     }
   }
 
-  function setupNode() {
-    // Node.js crypto-based RNG - http://nodejs.org/docs/v0.6.2/api/crypto.html
-    //
-    // Moderately fast, high quality
-    if ('function' === typeof require) {
-      try {
-        var _rb = require('crypto').randomBytes;
-        _nodeRNG = _rng = _rb && function() {return _rb(16);};
-        _rng();
-      } catch(e) {}
-    }
-  }
-
-  if (_window) {
-    setupBrowser();
-  } else {
-    setupNode();
-  }
+  setupBrowser();
 
   // Buffer class to use
-  var BufferClass = ('function' === typeof Buffer) ? Buffer : Array;
+  var BufferClass = Array;
 
   // Maps for number <-> hex string conversion
   var _byteToHex = [];
